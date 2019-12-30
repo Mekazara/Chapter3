1f110a932ee59baaa21a40c5b8be4f1fa5c61288
Mekazara <58605702+Mekazara@users.noreply.github.com>
2019-12-30 12:16:26 +0600

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,8 +1,7 @@
 print("Введите слова через пробелы")
-words = input("")
-list_of_words = list(words.split())
-list_of_words.sort(key=len)
-for word in list_of_words:
+words = input("").split()
+words.sort(key=len)
+for word in words:
     print(word, end=' ')
 
 
