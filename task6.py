6f694580728692e3925dad85d4e5ab559abb600a
Mekazara <58605702+Mekazara@users.noreply.github.com>
2019-12-30 14:08:11 +0600

    Add files via upload

--- task6.py
+++ task6.py
@@ -1,20 +1,23 @@
 def isBalanced(s):
-    ls = []
     opening_bracket = ["(", "[", "{"]
     closing_bracket = [")", "]", "}"]
+    ls = []
     for item in s:
         if item in opening_bracket:
             ls.append(opening_bracket.index(item))
-        elif item in closing_bracket:
-            if len(ls) == 0:
-                return "NO"
+        elif item in closing_bracket and len(ls) != 0:
             if ls[-1] == closing_bracket.index(item):
                 ls.pop()
             else:
-                return "NO"
-    return "YES"
-
-print(isBalanced("{}"))
+                ls.append(item)
+        elif item in closing_bracket:
+            ls.append(item)
+    if ls:
+        return "NO"
+    else:
+        return "YES"
+print(isBalanced("{(([])[])[]]}"))
+print(isBalanced("{[}"))
 print(isBalanced("}([[{)[]))]{){}["))
 print(isBalanced("{]]{()}{])"))
 print(isBalanced("(){}"))
